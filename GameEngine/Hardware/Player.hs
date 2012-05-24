bebcbd8791388c875ca35128f2db255c28485e96
Michael DeWitt <md@hackfoolery.com>
2012-05-24 01:40:42 -0400

    Add support for research to Players

--- GameEngine/Hardware/Player.hs
+++ GameEngine/Hardware/Player.hs
@@ -19,24 +19,34 @@ import Hardware.Zone
 -- | Player state consists of immutable nation and mutable income,
 -- balance, and purchased-yet-unplaced units.
 data Player = Player
-            { playerNation  :: Nation
-            , playerIncome  :: Int
-            , playerBalance :: Int
-            , unitsToPlace  :: Units
-            } deriving(Show)
+            { playerNation   :: Nation
+            , playerIncome   :: Int
+            , playerBalance  :: Int
+            , unitsToPlace   :: Units
+            , researchedTech :: [Research]
+            } deriving(Show,Read)
+
+instance Eq Player where
+  a == b = playerNation a == playerNation b
 
 -- | Updates player income. The input income is expected to be
 -- positive, as all valid incomes are.
 newPlayerIncome :: Int -> Player -> Player
-newPlayerIncome newIncome (Player n _ b u) = Player n newIncome b u
+newPlayerIncome newIncome (Player n _ b u r) =
+  Player n newIncome b u r
 
 -- | Updates player balance. Balance's also must be positive.
 newPlayerBalance :: Int -> Player -> Player
-newPlayerBalance newBal (Player n i _ u) = Player n i newBal u
+newPlayerBalance newBal (Player n i _ u r) =
+  Player n i newBal u r
 
 -- | Updates player units.
 newPlayerUnits :: Units -> Player -> Player
-newPlayerUnits units (Player n i b _) = Player n i b units
+newPlayerUnits units (Player n i b _ r) = 
+  Player n i b units r
+
+newPlayerResearch :: Research -> Player -> Player
+newPlayerResearch = undefined
 
 -- | Collects the player's income and adds it to their balance.
 collectIncome :: Player -> Player
@@ -50,7 +60,7 @@ collectIncome player =
 -- to be less than the player's balance. This implies that the
 -- client should check to make sure that it can afford the purchase.
 buyUnits :: Player -> Units -> Player
-buyUnits (Player n i b _) newUnits = Player n i b' newUnits
+buyUnits (Player n i b _ r) newUnits = Player n i b' newUnits r
   where b' = b - totalUnitsCost newUnits
         totalUnitsCost :: Units -> Int
         totalUnitsCost units =
@@ -80,3 +90,13 @@ placeUnits player units zone =
   in ( newPlayerUnits pu' player
      , newZoneUnits azu' zone
      )
+
+
+
+data Research = JetPower
+              | Rockets
+              | SuperSubs
+              | LongRangeAircraft
+              | IndustrialTech
+              | HeavyBombers
+                deriving(Show,Read,Eq,Ord)
