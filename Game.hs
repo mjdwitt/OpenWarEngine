9be238e0f29b9cf87e96eeae7411c82f13f98216
Michael DeWitt <md@hackfoolery.com>
2012-06-08 02:27:10 -0400

    Remove turn state param from Game modifiers.

--- Game.hs
+++ Game.hs
@@ -20,10 +20,9 @@ data Game = Game
           } deriving(Show,Read)
 
 -- Some modifiers for Game objects
-newRu ru' (Game _ de uk jp us b t)    = Game ru' de uk jp us b t
-newDe de' (Game ru _ uk jp us b t)    = Game ru de' uk jp us b t
-newUk uk' (Game ru de _ jp us b t)    = Game ru de uk' jp us b t
-newJp jp' (Game ru de uk _ us b t)    = Game ru de uk jp' us b t
-newUs us' (Game ru de uk jp _ b t)    = Game ru de uk jp us' b t
-newBoard b' (Game ru de uk jp us _ t) = Game ru de uk jp us b' t
-newTurn t' (Game ru de uk jp us b _)  = Game ru de uk jp us b t'
+newRu ru' (Game _ de uk jp us b)    = Game ru' de uk jp us b
+newDe de' (Game ru _ uk jp us b)    = Game ru de' uk jp us b
+newUk uk' (Game ru de _ jp us b)    = Game ru de uk' jp us b
+newJp jp' (Game ru de uk _ us b)    = Game ru de uk jp' us b
+newUs us' (Game ru de uk jp _ b)    = Game ru de uk jp us' b
+newBoard b' (Game ru de uk jp us _) = Game ru de uk jp us b'
